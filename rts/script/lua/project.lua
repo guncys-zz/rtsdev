91161e45ead4b17806a021329d9ad42f2fe37a7f
syota.yoshikai <syota.yoshikai@progmind.jp>
2016-03-08 15:31:49 +0900

    アセットとスクリプトの整理

--- rts/script/lua/project.lua
+++ rts/script/lua/project.lua
@@ -1,20 +1,21 @@
 -----------------------------------------------------------------------------------
--- This implementation uses the default SimpleProject and the Project extensions are 
+-- This implementation uses the default SimpleProject and the Project extensions are
 -- used to extend the SimpleProject behavior.
 
 -- This is the global table name used by Appkit Basic project to extend behavior
 Project = Project or {}
 
 require 'script/lua/flow_callbacks'
+require 'script/lua/tools'
 
 Project.level_names = {
-	empty = "content/levels/empty"
+	testmap = "content/levels/testmap1"
 }
 
 -- Can provide a config for the basic project, or it will use a default if not.
 local SimpleProject = require 'core/appkit/lua/simple_project'
 SimpleProject.config = {
-	standalone_init_level_name = Project.level_names.empty,
+	standalone_init_level_name = Project.level_names.testmap,
 	camera_unit = "core/appkit/units/camera/camera",
 	camera_index = 1,
 	shading_environment = nil, -- Will override levels that have env set in editor.
@@ -22,18 +23,22 @@ SimpleProject.config = {
 	exit_standalone_with_esc_key = true
 }
 
--- Optional function by SimpleProject after level, world and player is loaded 
+-- Optional function by SimpleProject after level, world and player is loaded
 -- but before lua trigger level loaded node is activated.
 function Project.on_level_load_pre_flow()
 end
 
--- Optional function by SimpleProject after loading of level, world and player and 
+-- Optional function by SimpleProject after loading of level, world and player and
 -- triggering of the level loaded flow node.
 function Project.on_level_shutdown_post_flow()
 end
 
 -- Optional function called by SimpleProject after world update (we will probably want to split to pre/post appkit calls)
 function Project.update(dt)
+    if stingray.Window then
+        stingray.Window.set_show_cursor(true)
+        stingray.Window.set_clip_cursor(false)
+    end
 end
 
 -- Optional function called by SimpleProject *before* appkit/world render
