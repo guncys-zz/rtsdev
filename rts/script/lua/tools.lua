b79339f3ea87bd466b3597e5c3a053d015a95438
syota.yoshikai <syota.yoshikai@progmind.jp>
2016-04-20 15:49:38 +0900

    CSVで読み込んだUnitが別のレベルに移動しても残ってしまう問題の解決。 レベルをshutdownするときにCSVで読み込んだUnitのインスタンスを削除するように変更。

--- rts/script/lua/tools.lua
+++ rts/script/lua/tools.lua
@@ -6,6 +6,9 @@ local Quaternion = stingray.Quaternion
 
 Tools = Tools or {}
 
+-- spawnしたオブジェクトをテーブル格納
+unit_objects = {}
+
 function Tools.Load_csv(t)
     
     print("csv path: "..t.Path)
@@ -16,17 +19,31 @@ function Tools.Load_csv(t)
         return 
     end
     
+    unit_objects = {}
     for line in file:lines() do
         print("start spawn: "..line)
-        name, px, py, pz, rx, ry, rz = string.match(line, "(.-)%,(.-)%,(.-)%,(.-)%,(.-)%,(.-)%,(.+)")
+        local name, px, py, pz, rx, ry, rz = string.match(line, "(.-)%,(.-)%,(.-)%,(.-)%,(.-)%,(.-)%,(.+)")
+        
         Tools.Spawn(name, Vector3(px, py, pz), Quaternion.from_euler_angles_xyz(rx, ry, rz))
     end
     
 end
 
+function Tools.Destroy_units_csv(t)
+    local SimpleProject = require 'core/appkit/lua/simple_project'
+    local world = SimpleProject.world
+    
+    for key, unit in ipairs(unit_objects) do
+        
+        -- csvで指定したunitを削除
+        stingray.World.destroy_unit(world, unit)
+    end
+end
+    
 function Tools.Spawn(name, position, rotation)
     local SimpleProject = require 'core/appkit/lua/simple_project'
     local world = SimpleProject.world
         
-    stingray.World.spawn_unit(world, name, position, rotation)
+    local unit = stingray.World.spawn_unit(world, name, position, rotation)
+    table.insert(unit_objects, unit)
 end
